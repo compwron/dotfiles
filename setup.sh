c37513fc7781a895d6627462c2406610f816ea01
Linda Goldstein <linda.goldstein@sonicdrivein.com>
2016-07-08 08:13:16 -0500

    Symlinking setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-cp .bash_profile ~/.bash_profile
-cp .gitconfig ~/.gitconfig
-cp Preferences.sublime-settings "/Users/$(whoami)/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+ln -s $(pwd)/.bash_profile ~/.bash_profile
+ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/Preferences.sublime-settings "/Users/$(whoami)/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
